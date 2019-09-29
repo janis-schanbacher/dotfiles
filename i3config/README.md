9470fe1cf0b09db03c53b79bc3adeef6442c9ec9
Janis Schanbacher <janis.schanbacher@posteo.de>
2019-09-29 00:52:20 -0700

    Add basic i3configurations.

---
+++ i3config/README.md
@@ -0,0 +1,10 @@
+# i3 configuration
+
+packages used:
+
+* i3-gaps
+* polybar
+* compton
+* xdotool
+
+sudo ln -s ~/dotfiles/i3config/create_i3_config.sh /usr/local/bin/create_i3_config.sh`
---
+++ i3config/create_i3_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+folderpath=$HOME"/dotfiles/i3config/"
+srcpath=$folderpath"src"
+filetype=".i3.config"
+config="$folderpath/i3.config"
+
+srcfiles=(
+# header
+set-modkey
+# focus_wrapping
+# font
+i3-gaps
+i3-colors
+polybar
+compton
+# background-image
+keybindings
+reload_restart
+hidemouse
+# scrolling
+screenshot
+media-hotkeys
+# spotify
+system_power_mode
+apps_and_boot
+manjaro_defaults
+autolock
+)
+
+rm $config
+touch $config
+
+for filename in "${srcfiles[@]}"
+do
+  cat "$srcpath/$filename.i3.config" >> $config
+done
+
+if [ "$MACHINE" == "WORK" ]
+then
+	cat $srcpath/work_related.i3.config >> $config
+fi
+
